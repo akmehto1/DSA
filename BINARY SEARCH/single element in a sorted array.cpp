7e31e40e020e2872a1992ec368048f3de1b9cc00
Abhishek Kumar Mehto <147036217+akmehto1@users.noreply.github.com>
2024-05-21 22:54:02 +0530

    Add files via upload
    single element in  sorted array

---
+++ BINARY SEARCH/single element in a sorted array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+           int start=0;
+           int end=nums.size()-1;
+
+           while(start<end){
+
+            int mid=start+(end-start)/2;
+            cout<<nums[mid]<<endl;
+            if(nums[mid]==nums[mid^1]){
+                start=mid+1;
+            }
+             else end=mid;
+           }
+           
+return nums[start];
+
+    }
+
+
+    
+};
